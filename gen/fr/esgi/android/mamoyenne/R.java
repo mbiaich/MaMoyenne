90efbbd50b9e475bd8dbff7ad9236c0b8b4676f1
freddy265 <sarhan.mohamed@live.fr>
2015-02-01 13:06:14 +0100

    design beginning

--- gen/fr/esgi/android/mamoyenne/R.java
+++ gen/fr/esgi/android/mamoyenne/R.java
@@ -23,12 +23,21 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int round_button=0x7f020001;
+        public static final int texview_design=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int gestionNotes=0x7f080000;
+        public static final int messageAccueil=0x7f080001;
+        public static final int progressBar1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int ajout_matiere=0x7f030001;
+        public static final int ajout_note=0x7f030002;
+        public static final int liste_matieres=0x7f030003;
+        public static final int notes_pour_matiere=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
