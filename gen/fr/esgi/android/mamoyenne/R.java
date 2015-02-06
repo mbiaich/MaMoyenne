96a0af945ad41c0d333f5e29eb33bec731a59303
M.SARHAN <sarhan.miohamed@live.fr>
2015-02-06 17:02:38 +0100

    design Commit without arrangemant

--- gen/fr/esgi/android/mamoyenne/R.java
+++ gen/fr/esgi/android/mamoyenne/R.java
@@ -22,22 +22,49 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int round_button=0x7f020001;
-        public static final int texview_design=0x7f020002;
+        public static final int bouton_arrondi=0x7f020000;
+        public static final int button_oval=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int textview_round=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int gestionNotes=0x7f080000;
-        public static final int messageAccueil=0x7f080001;
-        public static final int progressBar1=0x7f080002;
+        public static final int action_settings=0x7f08001b;
+        public static final int button1=0x7f08000a;
+        public static final int button2=0x7f080013;
+        public static final int coeffLabel=0x7f08000d;
+        public static final int coefficietMatiere=0x7f080004;
+        public static final int delete=0x7f08000f;
+        public static final int detailMatiereLabel=0x7f08000b;
+        public static final int editText1=0x7f080006;
+        public static final int editText2=0x7f080007;
+        public static final int editText3=0x7f080009;
+        public static final int gestionNotes=0x7f080016;
+        public static final int listView1=0x7f080019;
+        public static final int listeMatieres=0x7f080015;
+        public static final int listeMatieresLabel=0x7f080014;
+        public static final int messageAccueil=0x7f080017;
+        public static final int modify=0x7f08000e;
+        public static final int myenneLabel=0x7f080018;
+        public static final int nomMatiere=0x7f080002;
+        public static final int nomMatiereLabel=0x7f08000c;
+        public static final int progressBar1=0x7f08001a;
+        public static final int register=0x7f080005;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080001;
+        public static final int textView3=0x7f080003;
+        public static final int textView4=0x7f080008;
+        public static final int textView5=0x7f080010;
+        public static final int textView6=0x7f080011;
+        public static final int textView7=0x7f080012;
     }
     public static final class layout {
         public static final int activity_ajout_matiere=0x7f030000;
         public static final int activity_ajout_note=0x7f030001;
-        public static final int activity_liste_matieres=0x7f030002;
-        public static final int activity_main=0x7f030003;
-        public static final int activity_notes_pour_matiere=0x7f030004;
+        public static final int activity_detail_matiere=0x7f030002;
+        public static final int activity_details_note=0x7f030003;
+        public static final int activity_liste_matieres=0x7f030004;
+        public static final int activity_main=0x7f030005;
+        public static final int activity_notes_pour_matiere=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f070000;
