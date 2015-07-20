7f09d428b6cb38b1c474effc5f2f59c0860bfe17
lienafalionel <lienafa.lionel@gmail.com>
2015-07-20 10:45:17 +0200

    Appui long matière et note

--- src/fr/esgi/android/mamoyenne/ListeDesMatieres.java
+++ src/fr/esgi/android/mamoyenne/ListeDesMatieres.java
@@ -3,14 +3,21 @@ package fr.esgi.android.mamoyenne;
 import java.util.List;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ListView;
+import android.widget.Toast;
 import fr.esgi.android.mamoyenne.DAO.MatiereDAO;
 import fr.esgi.android.mamoyenne.adapters.MatiereListAdapter;
 import fr.esgi.android.mamoyenne.tables.Matiere;
@@ -26,10 +33,11 @@ public class ListeDesMatieres extends ListActivity {
 		matiereDao.open();
 		refresh();
 		setContentView(R.layout.activity_liste_matieres);
+		ListView l = (ListView) findViewById(android.R.id.list);
+		registerForContextMenu(l);
 		ActionBar actionBar = getActionBar();
 		actionBar.setTitle(R.string.subjectList);
 	}
-	
 
 	@Override
 	protected void onRestart() {
@@ -37,7 +45,6 @@ public class ListeDesMatieres extends ListActivity {
 		super.onRestart();
 		refresh();
 	}
-
 
 	public void frmAjoutMatiere(View v) {
 		this.startActivity(new Intent(this, AjoutDuneMatiere.class));
@@ -50,20 +57,67 @@ public class ListeDesMatieres extends ListActivity {
 			MatiereListAdapter adapter = new MatiereListAdapter(this, matieres);
 			setListAdapter(adapter);
 		}
-		
+
 	}
-	
+
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		Intent i = new Intent(this, NotesPourUneMatiere.class);
 		i.putExtra("idMatiere", id);
 		startActivity(i);
 	}
-	
+
 	@Override
-	public void onDestroy(){
+	public void onDestroy() {
 		matiereDao.close();
 		super.onDestroy();
 	}
 
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu_context, menu);
+
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		final Matiere m = (Matiere) getListAdapter().getItem(info.position);
+		if (item.getItemId() == R.id.delete) {
+			Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle(getString(R.string.dialogWarning));
+			builder.setMessage(getString(R.string.dialogAskDeleteField));
+			builder.setPositiveButton(getString(R.string.dialogYes),
+					new OnClickListener() {
+
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							Intent i = new Intent(ListeDesMatieres.this,
+									ListeDesMatieres.class);
+							matiereDao.deleteMatiere(m.getIdMatiere());
+							startActivity(i);
+							Toast.makeText(getApplicationContext(),
+									getString(R.string.deleteFieldToast),
+									Toast.LENGTH_LONG).show();
+						}
+					});
+			builder.setNegativeButton(getString(R.string.dialogNo),
+					new OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							// ne fait rien
+						}
+					});
+			builder.setIcon(android.R.drawable.ic_dialog_alert);
+			builder.show();
+		} else if (item.getItemId() == R.id.edit) {
+			Intent i = new Intent(this, DetailsMatiere.class);
+			i.putExtra("matiere", matiereDao.getMatiere(m.getIdMatiere()));
+			this.startActivity(i);
+		}
+		return super.onContextItemSelected(item);
+	}
 }
--- src/fr/esgi/android/mamoyenne/NotesPourUneMatiere.java
+++ src/fr/esgi/android/mamoyenne/NotesPourUneMatiere.java
@@ -3,31 +3,40 @@ package fr.esgi.android.mamoyenne;
 import java.util.List;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import fr.esgi.android.mamoyenne.DAO.MatiereDAO;
 import fr.esgi.android.mamoyenne.DAO.NoteDAO;
 import fr.esgi.android.mamoyenne.adapters.NoteListAdapter;
+import fr.esgi.android.mamoyenne.tables.Matiere;
 import fr.esgi.android.mamoyenne.tables.Note;
 
 public class NotesPourUneMatiere extends ListActivity {
 
-	private NoteDAO NoteDAO;
+	private NoteDAO noteDao;
 	private long idMatiere;
 	private MatiereDAO matiereDao;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		NoteDAO = new NoteDAO(this);
-		NoteDAO.open();
+		noteDao = new NoteDAO(this);
+		noteDao.open();
 		matiereDao = new MatiereDAO(this);
 		matiereDao.open();
 		idMatiere = getIntent().getExtras().getLong("idMatiere");
@@ -36,6 +45,8 @@ public class NotesPourUneMatiere extends ListActivity {
 		actionBar.setTitle(getString(R.string.markFor)+" " + matiereDao.getMatiere(idMatiere).getNom().toString());
 		
 		setContentView(R.layout.activity_notes_pour_matiere);
+		ListView l = (ListView) findViewById(android.R.id.list);
+		registerForContextMenu(l);
 		
 		//Affichage de la matière
 		TextView newTextMatiere = (TextView) findViewById(R.id.ajoutMatiereLabel);
@@ -43,7 +54,7 @@ public class NotesPourUneMatiere extends ListActivity {
 			    
 		//Affichage de la moyenne	
 		TextView newTextMoyenne = (TextView)findViewById(R.id.moyenneMatiereLabel);
-		newTextMoyenne.setText(NoteDAO.getMoyenneByMatiere(idMatiere) + " " + getString(R.string.mainGeneralMark));
+		newTextMoyenne.setText(noteDao.getMoyenneByMatiere(idMatiere) + " " + getString(R.string.mainGeneralMark));
 		
 		refresh();
 		
@@ -82,16 +93,10 @@ public class NotesPourUneMatiere extends ListActivity {
 		this.startActivity(i);
 	}
 	
-	public void viewDetailsMatiere(View v) {
-		Intent i = new Intent(this, DetailsMatiere.class);
-		i.putExtra("matiere", matiereDao.getMatiere(idMatiere));
-		this.startActivity(i);
-	}
-	
 
 	public void refresh() {
 		ListView listView = (ListView) findViewById(android.R.id.list);
-		List<Note> Notes = NoteDAO.getNotes(idMatiere);
+		List<Note> Notes = noteDao.getNotes(idMatiere);
 		if (!Notes.isEmpty()) {
 			NoteListAdapter adapter = new NoteListAdapter(this, Notes);
 			setListAdapter(adapter);
@@ -101,22 +106,66 @@ public class NotesPourUneMatiere extends ListActivity {
 		newTextMatiere.setText(getString(R.string.markFor) + " " + matiereDao.getMatiere(idMatiere).getNom().toString());
 		
 		TextView newTextMoyenne = (TextView)findViewById(R.id.moyenneMatiereLabel);
-		newTextMoyenne.setText(NoteDAO.getMoyenneByMatiere(idMatiere) + " " + getString(R.string.mainGeneralMark));
+		newTextMoyenne.setText(noteDao.getMoyenneByMatiere(idMatiere) + " " + getString(R.string.mainGeneralMark));
 	}
 	
-	@Override
+	/*@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		Intent i = new Intent(this, DetailsNote.class);
-		i.putExtra("note", NoteDAO.getNote((int) id));
+		i.putExtra("note", noteDao.getNote((int) id));
 		startActivity(i);
-	}
+	}*/
 	
 	
 	
 	@Override
 	public void onDestroy(){
-		NoteDAO.close();
+		noteDao.close();
 		matiereDao.close();
 		super.onDestroy();
+	}
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu_context, menu);
+
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		final Note n = (Note) getListAdapter().getItem(info.position);
+		if (item.getItemId() == R.id.delete) {
+			Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle(getString(R.string.dialogWarning));
+			builder.setMessage(getString(R.string.dialogAskDeleteMark));
+			builder.setPositiveButton(getString(R.string.dialogYes), new OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+		    	noteDao.deleteNote(n.getIdNote());
+					Intent i = new Intent(NotesPourUneMatiere.this, NotesPourUneMatiere.class);
+					i.putExtra("idMatiere", n.getIdMatiere());
+					startActivity(i);
+					
+					Toast.makeText(getApplicationContext(),getString(R.string.deletedMark), Toast.LENGTH_LONG).show();
+				}
+			});
+			builder.setNegativeButton(getString(R.string.dialogNo), new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+			// ne fait rien
+			}
+			});
+			builder.setIcon(android.R.drawable.ic_dialog_alert);
+			builder.show();
+	    } else if (item.getItemId() == R.id.edit) {
+			Intent i = new Intent(this, DetailsNote.class);
+			i.putExtra("note", noteDao.getNote((int) n.getIdNote()));
+			startActivity(i);
+		}
+		return super.onContextItemSelected(item);
 	}
 }
