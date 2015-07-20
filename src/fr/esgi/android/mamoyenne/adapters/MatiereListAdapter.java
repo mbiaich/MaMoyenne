956f26bbd20c5fe6e588f660d154a879a3e09454
lienafalionel <lienafa.lionel@gmail.com>
2015-07-20 09:51:15 +0200

    Tableau listView matières

--- src/fr/esgi/android/mamoyenne/adapters/MatiereListAdapter.java
+++ src/fr/esgi/android/mamoyenne/adapters/MatiereListAdapter.java
@@ -10,6 +10,8 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 import fr.esgi.android.mamoyenne.R;
+import fr.esgi.android.mamoyenne.DAO.MatiereDAO;
+import fr.esgi.android.mamoyenne.DAO.NoteDAO;
 import fr.esgi.android.mamoyenne.tables.Matiere;
 
 
@@ -18,11 +20,14 @@ public class MatiereListAdapter extends BaseAdapter {
 	private Context context;
 	private List<Matiere> matieres;
 	private LayoutInflater inflater;
+	private NoteDAO noteDAO;
 	
 	public MatiereListAdapter(Context context, List<Matiere> matieres) {
 		this.context = context;
 		this.matieres = matieres;
 		this.inflater = LayoutInflater.from(context);
+		this.noteDAO = new NoteDAO(context);
+		this.noteDAO.open();
 	}
 	
 	@Override
@@ -44,9 +49,17 @@ public class MatiereListAdapter extends BaseAdapter {
 	public View getView(int position, View convertView, ViewGroup parent) {
 		Matiere matiere = (Matiere) getItem(position);
 		convertView = inflater.inflate(R.layout.list_view_matiere, null);
-		TextView txtRowListMatiere = (TextView) convertView.findViewById(R.id.txt_row_liste_matiere);
-		txtRowListMatiere.setTextColor(Color.WHITE);
-		txtRowListMatiere.setText(matiere.getNom());
+		TextView txtRowListMatiereNomMatiere = (TextView) convertView.findViewById(R.id.txt_row_liste_matiere_nom_matiere);
+		txtRowListMatiereNomMatiere.setTextColor(Color.WHITE);
+		txtRowListMatiereNomMatiere.setText(matiere.getNom());
+		
+		TextView txtRowListMatiereCoeff = (TextView) convertView.findViewById(R.id.txt_row_liste_matiere_coeff);
+		txtRowListMatiereCoeff.setTextColor(Color.WHITE);
+		txtRowListMatiereCoeff.setText(String.valueOf(matiere.getCoefficient()));
+		
+		TextView txtRowListMatiereMoyenne = (TextView) convertView.findViewById(R.id.txt_row_liste_matiere_moyenne);
+		txtRowListMatiereMoyenne.setTextColor(Color.WHITE);
+		txtRowListMatiereMoyenne.setText(noteDAO.getMoyenneByMatiere(matiere.getIdMatiere()));
 		return convertView;
 	}
 
